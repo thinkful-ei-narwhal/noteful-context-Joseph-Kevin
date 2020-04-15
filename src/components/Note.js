db79453f9ae33bfa6542335305a636dd58d0b0f4
Kevin Jiang <kjiang95@gmail.com>
2020-04-15 11:36:08 -0500

    finished adding context

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import StateContext from './StateContext';
 
-export default function Note(props) {
-  return(
-    <div className="main">
-      <div id={props.id} className="note-item" >
-        <h2>{props.title}</h2>
-        <Link to={`note/${props.id}`}>Expand Me</Link>
-        <p>{props.modified}</p>
+
+
+export default class Note extends React.Component {
+  
+  static contextType = StateContext;
+
+  render() {
+    const {handleDelete} = this.context;
+    return(
+      <div className="main">
+        <div id={this.props.id} className="note-item" >
+          <h2>{this.props.title}</h2>
+          <Link to={`note/${this.props.id}`}>Expand Me</Link>
+          <p>{this.props.modified}</p>
+          <button type='button' className='deleteButton' onClick={() => handleDelete(this.props.id)}>Delete Me!</button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+
 }
--- src/components/constants/Sidebar.js
+++ src/components/constants/Sidebar.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import StateContext from '../StateContext';
+import { render } from '@testing-library/react';
 
-export default function Sidebar(props) {
-  let folderLinks = props.state.folders.map(folder => (
-  <span className="nav-item"><NavLink key={folder.id} to={`/folders/${folder.id}`} activeClassName="selected">{folder.name}</NavLink></span>
-  ))
 
-  return(
-    <div className="sidebar">
-      <nav>
-        {folderLinks}
-      </nav>
-      <button>Add Folder</button>
-    </div>
-    
-  )
+export default class Sidebar extends React.Component {
+
+  static contextType = StateContext;
+
+  render() {
+    const {folders} = this.context;
+    let folderLinks = folders.map(folder => (
+      <span className="nav-item"><NavLink key={folder.id} to={`/folders/${folder.id}`} activeClassName="selected">{folder.name}</NavLink></span>
+      ))
+
+    return(
+      <div className="sidebar">
+        <nav>
+          {folderLinks}
+        </nav>
+        <button>Add Folder</button>
+      </div>
+    )
+  }
+
 }
